a90e423e29a239392bc7a7c46a0dfdc698b5c3e0
Ninad1306 <ninad_1063@yahoo.com>
2025-04-11 19:50:10 +0530

    fix: change report name
    (cherry picked from commit e0fa8904fe28171a9d34781d7c84a97451a5e773)

--- india_compliance/gst_india/report/hsn_wise_summary_of_inward_supplies/hsn_wise_summary_of_inward_supplies.js
+++ india_compliance/gst_india/report/hsn_wise_summary_of_inward_supplies/hsn_wise_summary_of_inward_supplies.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Resilient Tech and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["HSN-wise-summary of purchases"] = {
+frappe.query_reports["HSN-wise-summary of inward supplies"] = {
     filters: [
         {
             fieldname: "company",
@@ -57,28 +57,4 @@ frappe.query_reports["HSN-wise-summary of purchases"] = {
             reqd: 1,
         },
     ],
-    onload: report => {
-        report.page.add_inner_button(__("Download JSON"), function () {
-            var filters = report.get_values();
-
-            frappe.call({
-                method: "india_compliance.gst_india.report.hsn_wise_summary_of_purchases.hsn_wise_summary_of_purchases.get_json",
-                args: {
-                    data: report.data,
-                    report_name: report.report_name,
-                    filters: filters,
-                },
-                callback: function (r) {
-                    if (r.message) {
-                        const args = {
-                            cmd: "india_compliance.gst_india.report.hsn_wise_summary_of_outward_supplies.hsn_wise_summary_of_outward_supplies.download_json_file",
-                            data: r.message.data,
-                            report_name: r.message.report_name,
-                        };
-                        open_url_post(frappe.request.url, args);
-                    }
-                },
-            });
-        });
-    },
 };
